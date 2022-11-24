6e5b1d5cfe85641850b4d6f8bc3644738db27773
Alexis-Salas-Portugal <92900232+Alexis-Salas-Portugal@users.noreply.github.com>
2022-11-23 18:00:11 -0700

    Corre en mi pc
    cambios necesarios para correr en mi pc

--- src/database.js
+++ src/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const URI = 'mongodb://localhost/mern-crud-test';
+const URI = 'mongodb+srv://AlexisSalas:ContraSegura@cluster0.8poqh.mongodb.net/PruebasGPS?retryWrites=true&w=majority';
 
 mongoose.connect(URI)
   .then(db => console.log('Db is connected'))
